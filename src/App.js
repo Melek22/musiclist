bb8d9c15442d4ae9593eb05fe15e8a60101faeb4
Melek22 <stefan_hodan@yahoo.com>
2019-02-26 23:10:36 +0200

    Added LastFM API - working

--- src/App.js
+++ src/App.js
@@ -4,19 +4,38 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import {TextField, Button} from '@material-ui/core';
+import axios from 'axios';
 
+const API_URL = 'https://ws.audioscrobbler.com/2.0/?limit=5&format=json&method=artist.search&api_key=' + process.env.REACT_APP_LASTFM_APPKEY;
+
+const isEmplt = (str) => str.length === 0;
 class App extends Component {
   state = {
-    searchTerm: ''
+    searchTerm: '',
+    artists: []
   }
   onTextChange = (event) => {
     const value = event.target.value;
 
     this.setState({searchTerm: value});
   }
+
+search = (terms) => {
+  const request = API_URL + '&artist=' + terms;
+
+  console.log(request);
+
+  axios.get(request).then((response) => {
+    this.setState({ artists: response.data.results.artistmatches.artist});
+  })
+}
+
   onSearchClick = () => {
-    console.log(this.state.searchTerm);
+    this.search(this.state.searchTerm);
   }
+//https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=d000cff27039229f64111c30e925a415&format=json
+
+
   render() {
     return (
       <div className="App">
@@ -39,9 +58,13 @@ class App extends Component {
             </Toolbar>
            </AppBar>
         </header>
-        <div>
-          
-        </div>
+        <p>{process.env.REACT_APP_LASTFM_APPKEY}</p>
+        <ul>
+          {this.state.artists.map((artist) => {
+            return <li><span>{artist.name}</span></li>
+          })}
+        </ul>
+    
       </div>
     );
   }
